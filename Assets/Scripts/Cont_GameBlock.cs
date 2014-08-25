b96ac1c3f84a1717089ff96cc812699b676ca8f2
Iamkhova <iamkhova@gmail.com>
2014-08-25 19:44:22 -0400

    Commit number 0001
    Okay.. here is the "first" commit of this project. Ughh.. been trying to
    get the syncs right on this

---
+++ Assets/Scripts/Cont_GameBlock.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cont_GameBlock : MonoBehaviour 
+{
+	// Exposed Variables
+	public Sprite defaultGraphic;
+	public Sprite activeGraphic;
+	public string blockStringValue;
+
+	public enum GameBlockState
+	{
+		Inactive,
+		ActiveHidden,
+		ActiveExposed
+	}
+
+	public GameBlockState currentState;
+	public GameBlockState newState;
+
+	// Use this for initialization
+	void Start () {
+	
+		currentState = GameBlockState.Inactive;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentState != GameBlockState.ActiveHidden && newState == GameBlockState.ActiveHidden) 
+		{
+			gameObject.GetComponent<SpriteRenderer>().sprite = activeGraphic;
+			currentState = newState;
+
+		}
+
+		if (currentState != GameBlockState.Inactive && newState == GameBlockState.Inactive) 
+		{
+			gameObject.GetComponent<SpriteRenderer>().sprite = defaultGraphic;
+			currentState = newState;
+				
+		}
+	
+	}
+
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public Transform prefab;
+	public float gameboard_x = -7.0f;
+	public float gameboard_y = 2.0f;
+	public int maxNumRows = 14;
+	public int maxNumColumns = 4;
+
+	public GameObject[] gameBlockArray;
+
+	//Do upon Init
+	void Awake()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		buildGameBoard();  
+		gameBlockArray = GameObject.FindGameObjectsWithTag ("game_block");
+	
+	}// end of void START
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// This Dynamically Builds the GameBoard
+	void buildGameBoard()
+	{
+		int row = 0;
+		int col = 0;	
+
+		float xstart = gameboard_x;
+		float ystart = gameboard_y;
+
+		while (col < maxNumColumns) {
+
+			while (row < maxNumRows) {
+						Instantiate (prefab, new Vector3 (xstart + row, ystart - col, 0), Quaternion.identity);
+						row++;
+				}
+
+			row = 0;
+			col++;
+
+			Debug.Log("Col=" + col);
+		}
+	}// end of buildGameBoard
+}
